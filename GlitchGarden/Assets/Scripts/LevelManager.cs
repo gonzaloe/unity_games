a0b10d6359b8227a19d0fe6c1709614c4bf2b06d
gonzaloe <gonzalo.espeche@mercadolibre.com>
2017-01-08 20:46:43 -0300

    first commit of glitch garden

---
+++ GlitchGarden/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour
+{
+	public float autoLoadTime;
+
+	void Start ()
+	{
+		Invoke ("LoadNextLevel", autoLoadTime);
+	}
+
+	public void LoadLevel (string levelName)
+	{
+		Debug.Log ("Level load requested for: " + levelName);
+		Application.LoadLevel (levelName);
+	}
+
+	public void QuitLevel ()
+	{
+		Debug.Log ("Quit Game requested");
+		Application.Quit ();
+	}
+
+	public void LoadNextLevel ()
+	{
+		Application.LoadLevel (Application.loadedLevel + 1);
+	}
+}
